a37bb26d095a39ce8d875247c3db5786f3493427
Krystian Urbaniak <krystian.g.urbaniak@gmail.com>
2021-02-21 13:50:26 +0100

    Adding a player's choice

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var computerMove, randomNumber;
+var computerMove, randomNumber, playerMove, playerInput;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
@@ -8,4 +8,21 @@ if (randomNumber == '1') {
 } else if (randomNumber == '3') {
   computerMove = 'nożyce';
 }
+//printMessage('Mój ruch: ' + computerMove);
+
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  printMessage('Wybrano nieprawidłową opcję');
+  playerMove = 'kamień';
+}
+
+
 printMessage('Mój ruch: ' + computerMove);
+printMessage('Twój ruch: ' + playerMove);
