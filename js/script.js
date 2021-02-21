228d22d75d3f885c46b82a006e86512e95547ad3
Krystian Urbaniak <krystian.g.urbaniak@gmail.com>
2021-02-21 11:39:44 +0100

    Adding javascript

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove;
+var playerMove;
+computerMove = 'kamień';
+playerMove = 'papier';
+printMessage('Zagrałem ' + computerMove + '! Ty zagrałeś ' +playerMove+'!  To znaczy że wygrywasz!');
